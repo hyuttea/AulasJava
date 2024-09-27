5a5986aedcb1a1496e68fe09594f202e84fc5dfe
Rafael Gibertoni <rafa08gibertoni@gmail.com>
2024-09-27 09:29:12 -0300

    ex03

--- src/Aula02/Exemplos.java
+++ src/Aula02/Exemplos.java
@@ -2,6 +2,15 @@ package Aula02;
 
 public class Exemplos {
     public static void main(String[] args) {
+    int AnoNasc;
+    double peso;
+    char sexo;
+    boolean destro;
 
+    AnoNasc = 2005;
+    peso = 48;
+
+
+    AnoNasc++;
     }
 }
