ae59b2622ef1204458c75cb6b0af7d8512f34930
Rafael Gibertoni <rafa08gibertoni@gmail.com>
2024-10-03 09:32:15 -0300

    03/10/2014 - aulas variaveis e operadores

--- src/Aula02/Exemplos/Exemplo03.java
+++ src/Aula02/Exemplos/Exemplo03.java
@@ -1,4 +1,32 @@
 package Aula02.Exemplos;
 
+import java.util.Arrays;
+
 public class Exemplo03 {
+    public static void main(String[] args) {
+
+        int num1 = 3, num2 = 7;
+        boolean resposta;
+
+        resposta = num1 > num2;
+        System.out.println( num1+ ">" + num2 + "=" + resposta);
+
+        resposta = num1 < num2;
+        System.out.println( num1+ "<" + num2 + "=" + resposta);
+
+        resposta = num1 >= num2;
+        System.out.println( num1+ ">=" + num2 + "=" + resposta);
+
+        resposta = num1 <= num2;
+        System.out.println( num1+ ">=" + num2 + "=" + resposta);
+
+        resposta = num1 != num2;
+        System.out.println( num1+ "!=" + num2 + "=" + resposta);
+
+
+
+
+
+
+    }
 }
