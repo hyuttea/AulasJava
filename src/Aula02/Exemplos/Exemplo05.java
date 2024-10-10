82d5990aee8985dc7be5d7eb61d1a24f43b9f450
Rafael Gibertoni <rafa08gibertoni@gmail.com>
2024-10-10 08:24:17 -0300

    10/10 ex 04 e ex 05

---
+++ src/Aula02/Exemplos/Exemplo05.java
@@ -0,0 +1,35 @@
+package Aula02.Exemplos;
+    public class Exemplo5 {
+        public static void main(String[] args) {
+            int num1 = 0;
+            System.out.println("num1 = " + num1);
+
+            num1++;
+            System.out.println("num1 ++ = " + num1);
+
+            num1 += 20;
+            System.out.println("num1 += 20 = " + num1);
+
+            num1--;
+            System.out.println("num1 -- = " + num1);
+
+            num1 -= 5;
+            System.out.println("num1 -= = " + num1);
+
+            num1 *= 2;
+            System.out.println("num1 *= = " + num1);
+
+            num1 /= 2;
+            System.out.println("num1 /= = " + num1);
+
+
+            int num2 = 0;
+            System.out.println("num2 = " + num2);
+            num2 = num1++;
+            System.out.println("antes num2 = num1++ depois num2 = " + num2 + " num1 = " + num1);
+
+            num2 = ++num1;
+            System.out.println("antes num2 = num1++ depois num2 = " + num2 + " num1 = " + num1);
+
+        }
+    }
