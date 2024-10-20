e9bc18ac9fcd7f734bf40557f6bcc3ec6b2b63df
RafaelGibertoni <rafa08gibertoni@gmail.com>
2024-10-20 12:06:47 -0300

    20/10 - exercicio 04 (aula04)

--- src/Aula04/Exercicios/Exercicio04.java
+++ src/Aula04/Exercicios/Exercicio04.java
@@ -1,9 +1,24 @@
 package Aula04.Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o valor das vendas: R$");
+        double vendas = scanner.nextDouble();
+        double comissao;
 
+        if (vendas < 1000) {
+            comissao = vendas * 0.05;
+        }else if (vendas >= 1000 && vendas <= 5000) {
+            comissao = vendas * 0.10;
+        }else {
+            comissao = vendas * 0.15;
+        }
+        System.out.println("O valor da comissão é: R$" + comissao);
 
+        scanner.close();
     }
 }
---
+++ src/Aula04/Exercicios/Exercicio05.java
@@ -0,0 +1,7 @@
+package Aula04.Exercicios;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+
+    }
+}
