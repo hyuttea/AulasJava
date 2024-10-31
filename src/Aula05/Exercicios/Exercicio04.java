f1890066c39ddc8c14e302122d51ce86386e781d
RafaelGibertoni <rafa08gibertoni@gmail.com>
2024-10-31 08:52:08 -0300

    exercicio04

--- src/Aula05/Exercicios/Exercicio04.java
+++ src/Aula05/Exercicios/Exercicio04.java
@@ -6,8 +6,25 @@ public class Exercicio04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número inteiro positivo: ");
+        int num = scanner.nextInt();
 
+        for (int i = 1; i <= num; i++) {
+
+            boolean primo = true;
+
+            for (int j = 2; j < i; j++) {
+
+                if (i % j == 0) {
+                    primo = false;
+                }
+
+                if (primo) {
+                    System.out.println(i);
+
+
+                }
+            }
 
         }
     }
-
+}
