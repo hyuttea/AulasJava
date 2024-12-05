7ae2496331ecaed36ccf445355be620a267c21de
RafaelGibertoni <rafa08gibertoni@gmail.com>
2024-12-05 20:29:50 -0300

    exercicioc08 aula 4

--- src/Aula04/Exercicios/Exercicio08.java
+++ src/Aula04/Exercicios/Exercicio08.java
@@ -1,4 +1,43 @@
 package Aula04.Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio08 {
+    public static void main(String[] args) {
+        System.out.println("Avalie de 1 á 5 quão satisfeito está com o desempenho de Fulano? ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha uma opção ");
+        System.out.println("\t1- Precisa Melhorar ");
+        System.out.println("\t2- Abaixo da Média ");
+        System.out.println("\t3- Média ");
+        System.out.println("\t4- Acima da Média ");
+        System.out.println("\t5- Excelente ");
+
+        int avaliacao;
+        avaliacao = scanner.nextInt();
+        switch (avaliacao) {
+            case 1:
+                System.out.println("Agradecemos a Avaliação");
+                break;
+            case 2:
+                System.out.println("Agradecemos a Avaliação");
+                break;
+            case 3:
+                System.out.println("Agradecemos a Avaliação");
+                break;
+            case 4:
+                System.out.println("Agradecemos a Avaliação");
+                break;
+            case 5:
+                System.out.println("Agradecemos a Avaliação");
+                break;
+            default:
+                System.out.println("Opcao Invalida");
+
+                scanner.close();
+
+
+        }
+    }
 }
--- src/Aula04/Exercicios/Exercicio09.java
+++ src/Aula04/Exercicios/Exercicio09.java
@@ -1,4 +1,7 @@
 package Aula04.Exercicios;
 
 public class Exercicio09 {
+    public static void main(String[] args) {
+
+    }
 }
