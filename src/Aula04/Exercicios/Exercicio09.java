e2daceec3613bacf1bf61a9cac7238ff15217f3d
RafaelGibertoni <rafa08gibertoni@gmail.com>
2024-12-05 20:52:20 -0300

    exercicio09 aula 4

--- src/Aula04/Exercicios/Exercicio09.java
+++ src/Aula04/Exercicios/Exercicio09.java
@@ -1,7 +1,28 @@
 package Aula04.Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio09 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int horario;
 
+        System.out.println("Digite a hora atual para verificar a qual turno corresponde");
+        horario = scanner.nextInt();
+
+        if (horario >= 5 && horario <= 12) {
+            System.out.println("Manhã");
+        }else if (horario > 12 && horario <= 18) {
+            System.out.println("Tarde");
+        }else if (horario > 18 && horario <= 22) {
+            System.out.println("Noite");
+        }else if (horario < 22 && horario < 5) {
+            System.out.println("Madrugada");
+        } else {
+            System.out.println(" Invalido");
+
+            scanner.close();
+
+        }
     }
 }
